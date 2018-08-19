6317c20d7ef1d37aa21d1dbc23a2667d56070116
nickhstr <nickhstr@gmail.com>
2018-08-19 00:09:34 -0400

    feat: write address to file and send email

--- main.go
+++ main.go
@@ -1,5 +1,129 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/smtp"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 
+	"github.com/jordan-wright/email"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var lastIPDir = os.Getenv("LAST_IP_DIR")
+var lastIPFile = os.Getenv("LAST_IP_FILE")
+var pathToIPFile = filepath.Join(lastIPDir, lastIPFile)
+
+func main() {
+	ipAddress, err := getIPAddress()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Current IP Address: %s\n", ipAddress)
+
+	isNew := isNewIPAddress(ipAddress)
+	if !isNew {
+		fmt.Println("IP Address has not changed")
+		return
+	}
+
+	fmt.Println("IP Address has been updated")
+
+	if err = setNewIPAddress(ipAddress); err != nil {
+		fmt.Println(err)
+	}
+
+	emailMessage := fmt.Sprintf("Updated address: %s", ipAddress)
+
+	if err = sendEmail([]byte(emailMessage)); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getIPAddress() ([]byte, error) {
+	var ipAddress []byte
+
+	requestURL := "http://checkip.dyndns.org"
+	ipRegex, err := regexp.Compile(`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`)
+	if err != nil {
+		fmt.Println("Cannot compile regular expression")
+		return ipAddress, err
+	}
+
+	resp, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Println("Could not fetch request")
+		return ipAddress, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Could not read body")
+		return ipAddress, err
+	}
+
+	ipAddress = ipRegex.Find(body)
+
+	return ipAddress, nil
+}
+
+func isNewIPAddress(ipAddress []byte) bool {
+	rawLastAddress, err := ioutil.ReadFile(pathToIPFile)
+	if err != nil {
+		fmt.Printf("Failed to read '%s', creating new file\n", pathToIPFile)
+
+		err = os.MkdirAll(lastIPDir, os.ModePerm)
+		if err != nil {
+			fmt.Printf("Failed to create dir: %s\n", lastIPDir)
+		}
+
+		err = ioutil.WriteFile(pathToIPFile, ipAddress, 0644)
+		if err != nil {
+			fmt.Printf("Failed to create %s\n", lastIPFile)
+		}
+
+		return true
+	}
+
+	lastAddress := strings.TrimSpace(string(rawLastAddress))
+
+	return lastAddress != string(ipAddress)
+}
+
+func setNewIPAddress(ipAddress []byte) error {
+	err := ioutil.WriteFile(pathToIPFile, ipAddress, 0644)
+	if err != nil {
+		fmt.Println("Unable to write IP address")
+		return err
+	}
+
+	return nil
+}
+
+func sendEmail(message []byte) error {
+	fromAddress := os.Getenv("EMAIL_FROM_ADDRESS")
+	fromPassword := os.Getenv("EMAIL_FROM_PASSWORD")
+	toAddress := os.Getenv("EMAIL_TO_ADDRESS")
+
+	e := email.NewEmail()
+	e.From = fmt.Sprintf("Nick Hester <%s>", fromAddress)
+	e.To = []string{toAddress}
+	e.Subject = "New IP Address for Raspberry Pi"
+	e.Text = message
+
+	err := e.Send("smtp.gmail.com:587", smtp.PlainAuth("", fromAddress, fromPassword, "smtp.gmail.com"))
+	if err != nil {
+		fmt.Println("Failed to send email")
+		return err
+	}
+
+	return err
 }
